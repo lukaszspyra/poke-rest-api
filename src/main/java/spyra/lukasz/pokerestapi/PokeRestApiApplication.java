a7bcf3c0094cb5584ad7a4aee65fa7c87381a0ae
lukaszspyra <lukasz.spyrka@gmail.com>
2023-05-05 21:25:27 +0200

    Create application template for Spring's REST api

---
+++ src/main/java/spyra/lukasz/pokerestapi/PokeRestApiApplication.java
@@ -0,0 +1,13 @@
+package spyra.lukasz.pokerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spyra/lukasz/pokerestapi/PokeRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package spyra.lukasz.pokerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
