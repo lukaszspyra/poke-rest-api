de00f6790fcb0648690bb4069740f811b311a2e0
lukaszspyra <lukasz.spyrka@gmail.com>
2023-05-08 01:33:00 +0200

    Modify Pokemon domain to be properly handled by Rest Template
    Used @JsonSetter annotation to set not matching property from nested json node. Nested json node is mapped automatically to class Sprites, which is used by @JsonSetter to set non-matching domain field

--- src/main/java/spyra/lukasz/pokerestapi/shared/Pokemon.java
+++ src/main/java/spyra/lukasz/pokerestapi/shared/Pokemon.java
@@ -1,15 +1,32 @@
 package spyra.lukasz.pokerestapi.shared;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import lombok.Getter;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Pokemon {
+
+    @JsonIgnore
+    private static final Logger log = LoggerFactory.getLogger(Pokemon.class);
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonIgnore
     private Long id;
 
+    @JsonProperty("id")
     private long apiId;
 
     private String name;
@@ -18,13 +35,17 @@ public class Pokemon {
 
     private int weight;
 
+    //Todo: find another API with high-res pictures
     private String imageUrl;
+
+    @JsonSetter("sprites")
+    public void imageUrlFromNestedJson(Sprites sprites) {
+        log.debug("Setting image url from Json Sprites node");
+        imageUrl = sprites.getIcon();
+    }
 
     @ManyToMany
     private List<PokeAbility> abilities;
-
-    @ManyToOne
-    private PokeSpecies species;
 
     @ManyToMany
     private List<PokeStats> stats;
@@ -32,4 +53,13 @@ public class Pokemon {
     @ManyToMany
     private List<PokeType> types;
 
+}
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Sprites {
+
+    @JsonProperty("front_default")
+    private String icon;
 }
