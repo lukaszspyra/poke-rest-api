16ce7f20374d4b384faaeae676bf82d920b2e857
lukaszspyra <lukasz.spyrka@gmail.com>
2023-05-08 12:38:16 +0200

    Adjust mapping for PokeStats
    It can accept parameter from nested json by @JsonSetter annotation

--- src/main/java/spyra/lukasz/pokerestapi/shared/PokeStats.java
+++ src/main/java/spyra/lukasz/pokerestapi/shared/PokeStats.java
@@ -1,5 +1,8 @@
 package spyra.lukasz.pokerestapi.shared;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +16,7 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class PokeStats {
 
     @Id
@@ -21,5 +25,23 @@ public class PokeStats {
 
     private String name;
 
+    @JsonProperty("base_stat")
     private int value;
+
+    /**
+     * Sets statistics name from nested json node during unmarshalling
+     *
+     * @param stat
+     */
+    @JsonSetter("stat")
+    public void setNameFromStat(Stat stat) {
+        name = stat.getName();
+    }
+}
+
+@Getter
+@Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Stat {
+    private String name;
 }
