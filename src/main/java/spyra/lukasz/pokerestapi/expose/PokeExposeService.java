8a38d80bd28e958fb2f1433b6acb04838189e3e6
lukaszspyra <lukasz.spyrka@gmail.com>
2023-05-11 19:54:39 +0200

    Implement findAll() in service

--- src/main/java/spyra/lukasz/pokerestapi/expose/PokeExposeService.java
+++ src/main/java/spyra/lukasz/pokerestapi/expose/PokeExposeService.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import spyra.lukasz.pokerestapi.shared.Pokemon;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,9 +16,13 @@ class PokeExposeService {
     private static final Logger log = LoggerFactory.getLogger(PokeExposeService.class);
     private final PokeExposeRepository repository;
 
-    Optional<Pokemon> findById(Long id){
+    Optional<Pokemon> findById(Long id) {
         log.debug("Searching database for entity with given id");
         return repository.findById(id);
     }
 
+    List<Pokemon> findAll() {
+        log.debug("Searching database for all entities");
+        return repository.findAll();
+    }
 }
