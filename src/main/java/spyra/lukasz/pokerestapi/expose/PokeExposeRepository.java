df94a5ea2f6ed588143ab18311179d138b0faf98
lukaszspyra <lukasz.spyrka@gmail.com>
2023-05-11 21:22:29 +0200

    Implement projections for aggregate root of the repository
    Instead of returning all objects, used Spring's projections to get name and id only

--- src/main/java/spyra/lukasz/pokerestapi/expose/PokeExposeRepository.java
+++ src/main/java/spyra/lukasz/pokerestapi/expose/PokeExposeRepository.java
@@ -3,5 +3,15 @@ package spyra.lukasz.pokerestapi.expose;
 import org.springframework.data.jpa.repository.JpaRepository;
 import spyra.lukasz.pokerestapi.shared.Pokemon;
 
+import java.util.Collection;
+
 interface PokeExposeRepository extends JpaRepository<Pokemon, Long> {
+
+    Collection<ProjectIdAndName> findAllProjectedBy();
+}
+
+interface ProjectIdAndName {
+    Long getId();
+
+    String getName();
 }
