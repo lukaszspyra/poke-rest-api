12577cbe1153782b32ba0fcf1b5f501f669bf874
lukaszspyra <lukasz.spyrka@gmail.com>
2023-05-13 19:17:24 +0200

    Implement PokeDeleteController
    Returns 204 for success or 404 if entity with given id not found

---
+++ src/main/java/spyra/lukasz/pokerestapi/delete/PokeDeleteController.java
@@ -0,0 +1,33 @@
+package spyra.lukasz.pokerestapi.delete;
+
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Handles pokemon delete functionality
+ */
+@RestController
+@RequiredArgsConstructor
+class PokeDeleteController {
+
+    private static final Logger log = LoggerFactory.getLogger(PokeDeleteController.class);
+    private final PokeDeleteService service;
+
+    @DeleteMapping("/pokemon/{id}")
+    ResponseEntity<?> delete(@PathVariable Long id) {
+        log.debug("Deletes pokemon by setting field isDeleted=true");
+        if (service.deleteOne(id) == 0) {
+            log.debug("Pokemon's id " + id + " not found - delete cancelled");
+            return ResponseEntity.notFound().build();
+        }
+        log.debug("Pokemon's id " + id + " found - delete successful");
+        return ResponseEntity.noContent().build();
+    }
+
+}
