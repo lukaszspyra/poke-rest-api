cc395d84e470ae96eb42c3587b2a840b9ce39537
lukaszspyra <lukasz.spyrka@gmail.com>
2023-05-14 02:23:34 +0200

    Disallow multiple delete calls on already deleted resource

--- src/main/java/spyra/lukasz/pokerestapi/delete/PokeDeleteRepository.java
+++ src/main/java/spyra/lukasz/pokerestapi/delete/PokeDeleteRepository.java
@@ -12,11 +12,12 @@ public interface PokeDeleteRepository extends JpaRepository<Pokemon, Long> {
 
     /**
      * Performs "soft delete" by setting boolean field isDeleted to true
+     * Operation is performed only once, when {@link Pokemon#isDeleted()} returns false
      * @param id of pokemon to set as deleted
      * @return number of affected records
      */
     @Modifying
     @Transactional
-    @Query("update Pokemon p set p.isDeleted = true where p.id = :id")
+    @Query("update Pokemon p set p.isDeleted = true where p.id = :id and p.isDeleted = false")
     int softDeleteById(@NotNull Long id);
 }
