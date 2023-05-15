e1644485ba89161a8a2b834278b211c2395df2b8
lukaszspyra <lukasz.spyrka@gmail.com>
2023-05-15 17:37:19 +0200

    Add ApiLinkGenerator to ApiExecutor

--- src/main/java/spyra/lukasz/pokerestapi/expose/ApiExecutor.java
+++ src/main/java/spyra/lukasz/pokerestapi/expose/ApiExecutor.java
@@ -4,29 +4,31 @@ package spyra.lukasz.pokerestapi.expose;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
+import spyra.lukasz.pokerestapi.shared.Pokemon;
 
 import java.util.Objects;
 import java.util.stream.Stream;
 
 @Component
+@RequiredArgsConstructor
 @PropertySource("classpath:settings.properties")
 class ApiExecutor {
     private static final Logger log = LoggerFactory.getLogger(ApiExecutor.class);
     private final RestOperations restTemplate;
-    private final String apiEntryUrl;
+    private final ApiLinkGenerator linkGenerator;
 
-    public ApiExecutor(RestOperations restTemplate, @Value("${api.entry.url}")String apiEntryUrl) {
-        this.restTemplate = restTemplate;
-        this.apiEntryUrl = apiEntryUrl;
-    }
 
     public Stream<ProjectedIdAndName> pokemonProjectionsFromApiEntryEndpoint() {
         log.debug("Api call for resources in entry url");
-        return Objects.requireNonNull(restTemplate.getForObject(apiEntryUrl, PokemonListJson.class)).projectionFromJson();
+        return Objects.requireNonNull(restTemplate.getForObject(linkGenerator.getApiEntryUrl(), PokemonListJson.class)).projectionFromJson();
+    }
+
+    Pokemon pokemonInstanceFromApi(Long id) {
+        log.debug("Api call for single resource details url");
+        return restTemplate.getForObject(linkGenerator.generateSingleResourceLink(id), Pokemon.class);
     }
 
 }
